4ebaaf9e542221191e85dbf7e7568ccb1ba7a961
qxan <mehmet.enfa@gmail.com>
2023-04-04 18:26:21 +0300

    footer

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { social } from '../data/dummydata'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <footer>
+        {social.map((item) => (
+          <>
+            <i data-aos='zoom-in'>{item.icon}</i>
+          </>
+        ))}  
+        <p data-aos='zoom-in'>All Right Rescevend 2018</p>
+      </footer> 
+    </>
   )
 }
 
