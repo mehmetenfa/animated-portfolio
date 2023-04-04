4d35b143e9f0201e66d03c256dc35f37d1b69bed
qxan <mehmet.enfa@gmail.com>
2023-04-04 18:23:42 +0300

    hero first

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React from "react";
+import { home } from "../data/dummydata";
+import Typewritter from "typewriter-effect";
 
 export const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <>
+      <section className="hero">
+        {home.map((val, i) => (
+          <div className="heroContent">
+            <h3 className="fontSize" data-aos="fade-right">
+              {val.text}
+            </h3>
+            <h1></h1>
+          </div>
+        ))}
+      </section>
+    </>
+  );
+};
