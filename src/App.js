ceea53d2e0d900c57b3683263d93b8cd1b783270
qxan <mehmet.enfa@gmail.com>
2023-04-04 03:18:07 +0300

    appjs

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+import "./App.css";
+import { Pages } from "./components/pages/Pages";
+import { useEffect } from "react";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+  // aos
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
-    <div className="App">
-      whatsup
-    </div>
+    <>
+      <Pages />
+    </>
   );
 }
 
