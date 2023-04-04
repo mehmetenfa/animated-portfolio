d13c1a7996b4c294049bc1cb383d50e101f80dbd
qxan <mehmet.enfa@gmail.com>
2023-04-04 17:01:16 +0300

    header

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,23 +1,30 @@
-import React, { useState } from 'react'
-import { Menu } from '@mui/icons-material'
-import { Link } from 'react-router-dom'
-import { navlink } from '../data/dummydata'
-import logo from '../data/images/logo.png'
- 
+import React, { useState } from "react";
+import { Menu } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { navlink } from "../data/dummydata";
+import logo from "../data/images/logo.png";
+
 export const Header = () => {
-  const [responsive, setResponsive] = useState(false)
+  const [responsive, setResponsive] = useState(false);
   return (
     <>
       <header>
-        <div>
-          <div>
-            <img src={logo} alt="" data-aos='zoom-in-right' />
+        <div className="container flexsb">
+          <div className="logo">
+            <img src={logo} alt="" data-aos="zoom-in-right" />
           </div>
-          <div className={responsive ? 'hideMenu' : 'nav'}>
-              {}
+          <div className={responsive ? "hideMenu" : "nav"}>
+            {navlink.map((links, i) => (
+              <Link to={links.url} key={i} data-aos="zoom-in-left">
+                {links.text}
+              </Link>
+            ))}
           </div>
+          <button className="toggle" onClick={() => setResponsive(!responsive)}>
+            <Menu className="icon"/>
+          </button>
         </div>
       </header>
     </>
-  )
-}
+  );
+};
