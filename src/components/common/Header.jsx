91ef9edcfac7d427e3b2e8d70b65faa6454a8dfc
qxan <mehmet.enfa@gmail.com>
2023-04-04 05:31:01 +0300

    header first

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Menu } from '@mui/icons-material'
+import { Link } from 'react-router-dom'
+import { navlink } from '../data/dummydata'
+import logo from '../data/images/logo.png'
+ 
 export const Header = () => {
+  const [responsive, setResponsive] = useState(false)
   return (
-    <div>Header</div>
+    <>
+      <header>
+        <div>
+          <div>
+            <img src={logo} alt="" data-aos='zoom-in-right' />
+          </div>
+          <div className={responsive ? 'hideMenu' : 'nav'}>
+              {}
+          </div>
+        </div>
+      </header>
+    </>
   )
 }
