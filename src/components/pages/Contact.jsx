28b7ee162e573d296052b05954916e46f4009107
qxan <mehmet.enfa@gmail.com>
2023-04-05 07:34:21 +0300

    .

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -20,19 +20,25 @@ export const Contact = () => {
                   />
                 </div>
                 <input type="email" placeholder="Subject" data-aos="flip-up" />
-                <textarea name="" id="" cols="30" rows="10" data-aos='flip-down'></textarea>
-                <button data-aos='zoom-in-up'>Submit</button>
+                <textarea
+                  name=""
+                  id=""
+                  cols="30"
+                  rows="10"
+                  data-aos="flip-down"
+                ></textarea>
+                <button data-aos="zoom-in-up">Submit</button>
               </form>
             </div>
             <div className="left">
               {contact.map((item) => (
-                <div className="box" data-aos='zoom-in'>
+                <div className="box" data-aos="zoom-in">
                   <i>{item.icon}</i>
                   <p>{item.text1}</p>
                   <p>{item.text2}</p>
                 </div>
               ))}
-            </div>  
+            </div>
           </div>
         </div>
       </div>
